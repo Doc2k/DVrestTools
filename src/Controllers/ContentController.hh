5c4f49e37aee56e66cb6d1b12c33c30fcda13aaa
internetArtist <ch@denkvertrieb.de>
2016-09-30 12:12:14 +0200

    Initial

---
+++ src/Controllers/ContentController.hh
@@ -0,0 +1,18 @@
+<?hh //strict
+
+    namespace DVrestTools\Controllers;
+    use Plenty\Plugin\Controller;
+    use Plenty\Plugin\Templates\Twig;
+
+    class ContentController extends Controller
+    {
+      public function getToken(Twig $twig):string
+      {
+          return $twig->render('DVrestTools::content.getToken');
+      }
+
+      public function getStock(Twig $twig):string
+      {
+          return $twig->render('DVrestTools::content.getStock');
+      }
+    }
---
+++ src/Providers/TachAuchRouteServiceProvider.hh
@@ -0,0 +1,16 @@
+<?hh //strict
+
+    namespace DVrestTools\Providers;
+
+
+    use Plenty\Plugin\RouteServiceProvider;
+    use Plenty\Plugin\Routing\Router;
+
+    class DVrestToolsRouteServiceProvider extends RouteServiceProvider
+    {
+        public function map(Router $router):void
+        {
+          $router->get('auth','DVrestTools\Controllers\ContentController@getToken');
+          $router->get('getstock','DVrestTools\Controllers\ContentController@getStock');
+        }
+    }
