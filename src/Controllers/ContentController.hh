5a53094f5278a86de3a0b975161f977c312ac1c8
internetArtist <ch@denkvertrieb.de>
2016-10-26 14:05:18 +0200

    get

--- src/Controllers/ContentController.hh
+++ src/Controllers/ContentController.hh
@@ -2,14 +2,14 @@
     namespace DVrestTools\Controllers;
     use Plenty\Plugin\Controller;
     use Plenty\Plugin\Templates\Twig;
-      use Plenty\Plugin\Http\Request;
+    use Plenty\Plugin\Http\Request;
 
 
     class ContentController extends Controller
     {
       public function getToken(Twig $twig):string
       {
-          return $twig->render('DVrestTools::content.getToken', array('user' => $_GET["user"]));
+          return $twig->render('DVrestTools::content.getToken', array('user' => $this->request->get('user');));
       }
       public function getStock(Twig $twig):string
       {
