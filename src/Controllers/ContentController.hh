275d0321c6031da34ef0b46c38bcee57028cd7b3
internetArtist <ch@denkvertrieb.de>
2016-10-26 14:21:31 +0200

    nerv

--- src/Controllers/ContentController.hh
+++ src/Controllers/ContentController.hh
@@ -8,7 +8,18 @@
     class ContentController extends Controller
     {
       private Request $request;
-      
+      /**
+      * @param Request $request
+      */
+
+      public function __construct(Request $request)
+
+  {
+
+    $this->request = $request;
+    
+  }
+
       public function getToken(Twig $twig):string
       {
           return $twig->render('DVrestTools::content.getToken', array('user' => $request->get('user')));
