982458db509b487ec405861d6c8dca601910cf3e
internetArtist <ch@denkvertrieb.de>
2016-11-01 13:20:29 +0100

    testen

--- src/Controllers/ContentController.hh
+++ src/Controllers/ContentController.hh
@@ -3,30 +3,30 @@
     use Plenty\Plugin\Controller;
     use Plenty\Plugin\Templates\Twig;
     use Plenty\Plugin\Http\Request;
+    use Plenty\Modules\Item\DataLayer\Models;
 
 
-    class ContentController extends Controller
-    {
+    class ContentController extends Controller{
       private Request $request;
       /**
       * @param Request $request
       */
 
-      public function __construct(Request $request)
+      public function __construct(Request $request){
+        $this->request = $request;
 
-  {
-
-    $this->request = $request;
-
-  }
-
-      public function getToken(Twig $twig):string
-      {
-          return $twig->render('DVrestTools::content.getToken', array('user' => $this->request->get('user'), 'passw' => $this->request->get('password'), 'callb' => $this->request->get('callback')));
       }
-      public function getStock(Twig $twig):string
-      {
 
-          return $twig->render('DVrestTools::content.getStock');
+      public function getToken(Twig $twig):string{
+        return $twig->render('DVrestTools::content.getToken', array('user' => $this->request->get('user'), 'passw' => $this->request->get('password'), 'callb' => $this->request->get('callback')));
       }
+
+      public function getStock(Twig $twig, Models $layerModel):string{
+        $augabespalten =['itemDescription' => ['name1'], 'variationStock' => ['stockPhysical', 'stockNet']];
+        $itemFilter = ['itemBase.id' => $this->request->get('id')];
+        $itemParams = ['language' => 'de'];
+        $Ergebnis = $layerModel->search($augabespalten, $itemFilter, $itemParams);
+        return $twig->render('DVrestTools::content.getStock', array('bestand' => $Ergebnis, 'callb' => $this->request->get('callback')));
+      }
+
     }
