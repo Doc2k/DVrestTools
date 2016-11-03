afb0223fa77ab83df5d47060c3363c26ee6b3d99
internetArtist <ch@denkvertrieb.de>
2016-11-03 12:18:30 +0100

    stock scheisse

--- src/Controllers/ContentController.hh
+++ src/Controllers/ContentController.hh
@@ -24,7 +24,7 @@
         $augabespalten =[
           'itemDescription' => ['name1'],
           'variationBase' => ['id'],
-          'variationStock' => ['stockNet']
+          'variationStock' => ['warehouseId']
         ];
         $itemFilter = ['itemBase.hasId' => ['itemId' => [$this->request->get('id')]]];
         $itemParams = ['language' => 'de'];
