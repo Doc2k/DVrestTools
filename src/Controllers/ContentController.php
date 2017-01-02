f1cd84e04385039a3dbc699b41a7ddbdcf0a54f3
internetArtist <ch@denkvertrieb.de>
2017-01-02 11:01:32 +0100

    no header

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -1,4 +1,4 @@
-<?php header('content-type: application/json; charset=utf-8');
+<?php 
     namespace DVrestTools\Controllers;
     use Plenty\Plugin\Controller;
     use Plenty\Plugin\Templates\Twig;
