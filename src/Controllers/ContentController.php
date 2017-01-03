662cdefe5f2a7c6d5f327441fa7e381d205fede3
internetArtist <ch@denkvertrieb.de>
2017-01-03 13:06:52 +0100

    und nochmal

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -45,8 +45,8 @@
           $ergebnisse = array();
           $stockColumns= ['VariationStock' =>['stockNet', 'stockPhysical', 'warehouseId']];
           foreach($Ergebnis as $item){
-            echo $item->variationBase->id;
-            //$Stockergebnis= $repo2->listStockByWarehouse($item->variationBase->id, $stockColumns);
+            $variID = $item->variationBase->id;
+            $Stockergebnis= $repo2->listStockByWarehouse($variID, $stockColumns);
             //foreach($Stockergebnis as $bestand){
               //$item+=['Stock_netto' => $bestand->stockNet];
               //$item+=['Stock_Phys' => $bestand->stockPhysical];
