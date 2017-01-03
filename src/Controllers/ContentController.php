7d6a61c1ec61e5bf711d1d54c0325b8a8804d328
internetArtist <ch@denkvertrieb.de>
2017-01-03 12:03:45 +0100

    bla

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -45,12 +45,14 @@
           $ergebnisse = array();
           $stockColumns= ['itemId', 'stockNet', 'stockPhysical', 'warehouseId'];
           foreach($Ergebnis as $item){
-            $Stockergebnis= $repo2->listStockByWarehouse($item->variationBase->id, $stockColumns);
-            foreach($Stockergebnis as $bestand){
+            echo $item->variationBase->id;
+            //$Stockergebnis= $repo2->listStockByWarehouse($item->variationBase->id, $stockColumns);
+            //foreach($Stockergebnis as $bestand){
               //$item['Stock_netto']=$bestand->stockNet;
               //$item['Stock_Phys']=$bestand->stockPhysical;
-              $ergebnisse[] = $bestand;
-            }
+              //$ergebnisse[] = $bestand;
+            //}
+            $ergebnisse[] = $item;
           }
 
           $myData= array(
