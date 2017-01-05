ce76162d0bf2c0b75aa0df07c24ab8566b11bd70
internetArtist <ch@denkvertrieb.de>
2017-01-05 13:36:30 +0100

    header entfernt, warehouse aus get

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -62,9 +62,7 @@
       // Set Stock
       // ----------------------------------------------------
         public function getStock2(VariationStockRepositoryContract $repo1){
-          header('content-type: application/json; charset=utf-8');
-          header("access-control-allow-origin: *");
-          $correctColumns=['warehouseId'=>103, 'quantity'=>$this->request->get('quant'), 'storageLocationId'=>0];
+          $correctColumns=['warehouseId'=>$this->request->get('warehouse'), 'quantity'=>$this->request->get('quant'), 'storageLocationId'=>0];
           // echo($this->request->get('callback')."({'success': 'true', 'newStock' : ".$this->request->get('quant')."})");
           $repo1->correctStock(1017, $correctColumns);
       // ----------------------------------------------------
