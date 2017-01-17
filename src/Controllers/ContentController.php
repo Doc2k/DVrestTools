3fd711101bd212c742a87a250e7ed83e871ecaf1
internetArtist <ch@denkvertrieb.de>
2017-01-17 18:43:47 +0100

    und nochmal mit Twig

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -61,7 +61,7 @@
 
       // Set Stock
       // ----------------------------------------------------
-        public function setStock(VariationStockRepositoryContract $repo1){
+        public function setStock(Twig $twig, VariationStockRepositoryContract $repo1){
           header('content-type: application/json; charset=utf-8');
           header("access-control-allow-origin: *");
           $correctColumns=['warehouseId'=>$this->request->get('warehouse'),'variationId'=>$this->request->get('variation_id'), 'quantity'=>$this->request->get('quant'), 'storageLocationId'=>0];
@@ -72,9 +72,9 @@
             'menge' => $this->request->get('quant'),
             'callb' => $this->request->get('callback')
           );
-          $ausgeben= $this->request->get('callback')."({'success': 'true', 'newStock' : ".$this->request->get('quant')."})";
-          return $ausgeben;
-          //return $twig->render('DVrestTools::content.setStock', $myData);
+          // $ausgeben= $this->request->get('callback')."({'success': 'true', 'newStock' : ".$this->request->get('quant')."})";
+          // return $ausgeben;
+          return $twig->render('DVrestTools::content.setStock', $myData);
       // ----------------------------------------------------
     }
 }
