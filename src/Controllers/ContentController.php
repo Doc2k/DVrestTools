c213517ce8ca5b97e1f967502478c754bd41ed55
internetArtist <ch@denkvertrieb.de>
2017-02-28 14:45:00 +0100

    dfgffdg

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -86,10 +86,10 @@
             'variationBase' => ['id', 'itemId', 'variationName', 'limitOrderByStockSelect', 'autoStockVisible', 'autoStockInvisible', 'active', 'availability', 'mainWarehouse'],
             'variationStock' => ['stockNet', 'stockPhysical', 'warehouseId']
           ];
-          // $itemFilter = ['isVisibleForClient'=> ['clientId' => [0]]];
           $itemFilter = ['itemBase.hasId' => ['itemId' => [$this->request->get('id')]]];
           $itemParams = ['language' => 'de', 'type' => 'warehouseId', 'warehouseId' => $this->request->get('warehouse')];
-
+          echo $this->request->get('id')\r;
+          echo $this->request->get('warehouse');
           $Ergebnis = $repo->search($augabespalten, $itemFilter, $itemParams);
           $ergebnisse = array();
           foreach($Ergebnis as $item){
